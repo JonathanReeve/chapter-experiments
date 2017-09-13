2281d6b28ceb002a3d67bf2031d64eaf22b15f16
Jonathan Reeve <jon.reeve@gmail.com>
2017-09-12 22:08:28 -0400

    better program metadata

--- chapterize.hs
+++ chapterize.hs
@@ -1,20 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 module Chapterize where
 import System.Console.CmdArgs
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
+import Data.Text hiding (map) 
 
-data ShowFile = ShowFile {file :: Maybe FilePath}
+data CLI = CLI {file :: Maybe FilePath}
               deriving (Show, Data, Typeable)
 
-showFile = ShowFile
+cli :: CLI
+cli = CLI
   {file = def &= typ "FILE" &= argPos 0}
+  &= verbosity
+  &= program "chapterize"
+  &= help "Break a text into chapters."
+  &= summary "Chapterize v2.0.1, (C) Jonathan Reeve"
 
+main :: IO ()
 main = do
-    options <- cmdArgs showFile
+    options <- cmdArgs cli
     contents <- case options of
-        ShowFile { file = Nothing } -> getContents
-        ShowFile { file = Just f  } -> readFile f
+        CLI { file = Nothing } -> getContents
+        CLI { file = Just f  } -> readFile f
     links <- runX $ parseHtml contents >>> css "h2" /> getText
-    putStr contents
+    -- putStr contents
+    -- TODO: strip out whitespace from links. Should be something like this. 
+    -- newlinks <- map strip links
+    -- print links
     mapM_ putStr links
