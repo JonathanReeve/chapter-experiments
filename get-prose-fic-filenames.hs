18f95bd324a16712f7fcc65ce5853c380cd1de05
Jonathan Reeve <jon.reeve@gmail.com>
2017-09-24 13:04:57 -0400

    script for getting prose fiction html file paths

--- get-prose-fic-filenames.hs
+++ get-prose-fic-filenames.hs
@@ -1,5 +1,17 @@
+import Text.Regex.PCRE
+
 main :: IO ()
 main = do
-  contents <- readFile "pg-prosefic-ids.txt"
-  let proseFicIDs = lines contents 
-  print proseFicIDs
+  proseFicFile <- readFile "pg-prose-fic-ids.txt"
+  let proseFicIDs = lines proseFicFile 
+  let isProseFicID bookID = bookID `elem` proseFicIDs
+  let isProseFic bookPath = isProseFicID $ getID bookPath
+  htmlFilesFile <- readFile "html-files.txt"
+  let htmlFilesPaths = lines htmlFilesFile
+  let proseFicPaths = filter isProseFic htmlFilesPaths
+  writeFile "pg-prose-fic-html-files.txt" $ unlines proseFicPaths
+
+getID :: String -> String
+getID str = do
+  let results = str =~ "_([0-9]+)/" :: [[String]]
+  concatMap last results
